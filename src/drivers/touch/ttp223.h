7bf4b5217ec510092986430720d08a163bfdb2e5
Matthew Baggett <matthew@baggett.me>
2020-01-31 15:37:49 +0100

    Working interrupt driven ttp223 driver

--- src/drivers/touch/ttp223.h
+++ src/drivers/touch/ttp223.h
@@ -2,29 +2,36 @@
 
 #pragma message("Included TTP223")
 
+void interrupt_ttp223();
+
 class TTP223
 {
 public:
     TTP223(EventsManager &eventsManager);
-    static void interrupt();
     void loop();
+    void enable();
+    void disable();
+    static void interrupt();
+    static bool interruptTriggered;
+    static bool hasInterruptOccured();
 
 private:
-    static boolean interruptTriggered;
     EventsManager _events;
 };
 
+bool TTP223::interruptTriggered = false;
+
 TTP223::TTP223(EventsManager &eventsManager) : _events(eventsManager)
 {
-    _events.trigger("ttp223_setup");
 // If we have a power pin to drive this sensor, enable it
 #ifdef PIN_POWER_TTP223
+#pragma message("TTP223's PIN_POWER_TTP223 is defined, so we must provide power to drive the TTP223 chip")
     pinMode(PIN_POWER_TTP223, OUTPUT);
-    digitalWrite(PIN_POWER_TTP223, HIGH);
+    enable();
 #endif
     pinMode(PIN_INTERRUPT_TTP223, INPUT);
-    attachInterrupt(PIN_INTERRUPT_TTP223, interrupt, RISING);
-    _events.trigger("ttp223_setup2");
+    attachInterrupt(PIN_INTERRUPT_TTP223, TTP223::interrupt, RISING);
+    _events.trigger("ttp223_ready");
 };
 
 void TTP223::interrupt()
@@ -32,10 +39,36 @@ void TTP223::interrupt()
     interruptTriggered = true;
 };
 
-void TTP223::loop()
+bool TTP223::hasInterruptOccured()
 {
     if (interruptTriggered)
+    {
+        interruptTriggered = false;
+        return true;
+    }
+    return false;
+}
+
+void TTP223::loop()
+{
+    if (TTP223::hasInterruptOccured())
     {
         _events.trigger("ttp223");
     }
 };
+
+void TTP223::enable()
+{
+    _events.trigger("ttp223_enable");
+#ifdef PIN_POWER_TTP223
+    digitalWrite(PIN_POWER_TTP223, HIGH);
+#endif
+}
+
+void TTP223::disable()
+{
+    _events.trigger("ttp223_disable");
+#ifdef PIN_POWER_TTP223
+    digitalWrite(PIN_POWER_TTP223, LOW);
+#endif
+}
