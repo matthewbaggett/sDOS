2e2f57892465d3e312268d2d7a4720da84ccc297
Matthew Baggett <matthew@baggett.me>
2020-02-07 05:33:46 +0100

    force set cpu to 10mhz by default from boot.

--- src/kern.hpp
+++ src/kern.hpp
@@ -83,6 +83,7 @@ String sDOS::getDriverID(String name)
 
 void sDOS::Setup()
 {
+    setCpuFrequencyMhz(10);
     Serial.begin(SERIAL_BAUD);
     Serial.setDebugOutput(SERIAL_DEBUG_ENABLE);
     delay(300);
