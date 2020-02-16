a0894c16726cf27d060f8e32abcbe288afe748f8
Matthew Baggett <matthew@baggett.me>
2020-02-16 14:56:05 +0100

    Allow disabling of serial all together.

--- src/debugger.hpp
+++ src/debugger.hpp
@@ -6,47 +6,66 @@
 #include <list>
 #define DEBUG_SERIAL_BUFFER_SIZE 255
 
+#ifndef SDOS_SERIAL_DEBUG_ENABLED
+#define SDOS_SERIAL_DEBUG_ENABLED true
+#endif
+
 bool sdos_is_wifi_active();
 #if defined(BLUETOOTH_ENABLED) && defined(ESP32)
 bool sdos_is_bluetooth_active();
 #endif
 
-using namespace std;
-using handlersList = std::list<void(*)(String message)>;
+#if SDOS_SERIAL_DEBUG_ENABLED == true
+    using namespace std;
+    using handlersList = std::list<void(*)(String message)>;
+#endif
 
+#ifndef SDOS_SERIAL_DEBUG_UNDERLYING_SYSTEM_DEBUG
+#define SDOS_SERIAL_DEBUG_UNDERLYING_SYSTEM_DEBUG false
+#endif
 
 class Debugger
 {
     public:
         Debugger()
         {
-            _serial = Serial;
-            _serial.begin(SERIAL_BAUD);
-            _serial.setDebugOutput(SERIAL_DEBUG_ENABLE);
+            #if SDOS_SERIAL_DEBUG_ENABLED == true
+                _serial = Serial;
+                _serial.begin(SERIAL_BAUD);
+                _serial.setDebugOutput(SDOS_SERIAL_DEBUG_UNDERLYING_SYSTEM_DEBUG);
+            #endif
         };
         void Debug(String component, String message, ...);
         unsigned int Step(){
-            Debugger::_stepCount++;
-            Debugger::Debug("step", "%d", Debugger::_stepCount);
-            return Debugger::_stepCount;
+            #if SDOS_SERIAL_DEBUG_ENABLED == true
+                Debugger::_stepCount++;
+                Debugger::Debug("step", "%d", Debugger::_stepCount);
+                return Debugger::_stepCount;
+            #endif
         };
         void addHandler(void (*newHandler)(String message))
         {
-            Debugger::_handlers.push_back(newHandler);
+            #if SDOS_SERIAL_DEBUG_ENABLED == true
+                Debugger::_handlers.push_back(newHandler);
+            #endif
         };
 
     private:
-        HardwareSerial _serial = Serial;
-        static String lastComponent;
-        static char lastBuff[DEBUG_SERIAL_BUFFER_SIZE];
-        static int duplicates;
-        static handlersList  _handlers;
-        static unsigned int _stepCount;
+            #if SDOS_SERIAL_DEBUG_ENABLED == true
+            HardwareSerial _serial = Serial;
+            static String lastComponent;
+            static char lastBuff[DEBUG_SERIAL_BUFFER_SIZE];
+            static int duplicates;
+            static handlersList  _handlers;
+            static unsigned int _stepCount;
+        #endif
 };
+#if SDOS_SERIAL_DEBUG_ENABLED == true
+    String Debugger::lastComponent;
+    char Debugger::lastBuff[DEBUG_SERIAL_BUFFER_SIZE];
+    int Debugger::duplicates = 0;
+    handlersList Debugger::_handlers;
+    unsigned int Debugger::_stepCount;
+#endif
 
-String Debugger::lastComponent;
-char Debugger::lastBuff[DEBUG_SERIAL_BUFFER_SIZE];
-int Debugger::duplicates = 0;
-handlersList Debugger::_handlers;
-unsigned int Debugger::_stepCount;
 #endif
